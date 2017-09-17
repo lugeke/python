b292d8fab4cd33af43086ea1d4f47e053c8ee1a9
jiaheng <lugeke@gmail.com>
2017-09-17 13:02:29 +0800

    use context manager for file operation

--- log.py
+++ log.py
@@ -1,7 +1,6 @@
 content = '-->'
 file = r'G:\PTXLocalMirror\{cf9e550b86f6e050a7faa810da961503}\LOG\日志_EPPortalnew_2016-01-20.LOG'
-f = open(file)
-for  line in f:
-    if content in line:
-        print(line)
-
+with open(file) as f:
+    for line in f:
+        if content in line:
+            print(line)
