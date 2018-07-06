5dd4a0b5056b481cdc3f651b076f305ce37f2fd4
陆嘉恒 <jiaheng@lujiahengs-MBP.lan>
2018-07-06 17:00:32 +0800

    use genetor to get magnet info

--- tools/getMagnets.py
+++ tools/getMagnets.py
@@ -1,33 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-url = 'https://www.seedmm.com/star/n4r'
 
+# url => html => content
+url = 'https://bt2.bt87.cc/index.php?r=files%2Findex&kw=Natalia+Starr+rarbg+1080p&page={}'
+def url_genetor(url, start, end):
+    for i in range(start, end+1):
+        yield url.format(i)
 
-def get_movie_links(url, end):
-    for i in range(1, end+1):
-        page = requests.get('%s/%d' % (url, i)).text
-        soup = BeautifulSoup(page, 'html.parser')
-        for tag in soup.find_all(class_='movie-box'):
-            yield tag['href']
+def html_genetor(urls):
+    for url in urls:
+        yield requests.get(url).text
 
+def content_genetor(htmls):
+    for h in htmls:
+        soup = BeautifulSoup(h, 'html.parser')
+        lis = soup.find_all('li', class_='col-xs-12 list-group-item')
+        for li in lis:
+            hashcode = 'magnet:?xt=urn:btih:{}'.format(li.a['href'][-40:])
+            name = li.a.string
+            size = li.find(class_='label label-warning').string
+            yield (name, hashcode, size)
+            
 
-# https://btso.pw/search/MEYD-063 ->
-def toMagnets(url):
-    response = requests.get(url)
-    page = response.text
-    soup = BeautifulSoup(page, 'html.parser')
-    magnets = []
-    for a in soup.find_all('a', href=re.compile('https://btso.pw/magnet/detail/hash/')):
-        magnet = a['href'].split('/')[-1]
-        size = a.findChildren('div')[1].text
-        magnets.append((magnet, size))
-    return magnets
-
-
-print(toMagnets('https://btso.pw/search/MEYD-063'))
-
-# for movie_link in get_movie_links(url, 2):
-#     print(movie_link)
-# for magnet in get_magnets('https://www.seedmm.com/MEYD-262'):
-#     print(magnet)
+urls = url_genetor(url, 1, 10)
+htmls = html_genetor(urls)
+contents = content_genetor(htmls)
+contents = list(contents)
+print(contents)
