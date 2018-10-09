b90910a9783da5b640ebe9edd0a5415161807696
jiaheng <lugeke@gmail.com>
2018-10-09 12:31:56 +0800

    add clock decorator to compute the running time of a funtion

--- decorators.py
+++ decorators.py
@@ -1,23 +1,20 @@
-def a_new_docorator(func):
+import time
+import functools
 
-	def wrapTheFunction():
-		print("I am doing some boring work before executing func")
-		func()
-		print("I am doing some boring work after executing func")
-	return wrapTheFunction
-
-def function_requiring_decoration():
-	print("I am the function which needs some decoration to remove my foul smell")
-
-function_requiring_decoration()
-
-function_requiring_decoration = a_new_docorator(function_requiring_decoration)
-
-function_requiring_decoration()
-
-
-@a_new_docorator
-def function_requiring_decoration():
-	print("I am the function which needs some decoration to remove my foul smell")
-
-function_requiring_decoration()
+def clock(func):
+	@functools.wraps(func)
+	def clocked(*args, **kwargs):
+		t0 = time.time()
+		result = func(*args, **kwargs)
+		elapsed = time.time() - t0
+		name = func.__name__
+		arg_lst = []
+		if args:
+			arg_lst.append(', '.join(repr(arg) for arg in args))
+		if kwargs:
+			pairs = ['%s=%r' %(k,w) for k,w in sorted(kwargs.items())]
+			arg_lst = arg_lst.append(', '.join(pairs))
+		arg_str = ', '.join(arg_lst)
+		print('[%0.8fs] %s(%s) -> %r' %(elapsed, name, arg_str, result))
+		return result
+	return clocked
