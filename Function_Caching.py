62c42b4a73ec3e0d3720da68f0447ac9999f57d2
jiaheng <lugeke@gmail.com>
2018-10-09 12:34:32 +0800

    use clock decorator to show the difference  of using lru_cache()

--- Function_Caching.py
+++ Function_Caching.py
@@ -1,9 +1,22 @@
 from functools import lru_cache
 
-@lru_cache(maxsize=32)
+from decorators import clock
+@clock
+def fib(n):
+	if n < 2:
+		return n
+	return fib(n-1) + fib(n-2)
+ 
+
+fib(10)
+print('---->')
+
+
+@lru_cache()
+@clock
 def fib(n):
 	if n < 2:
 		return n
 	return fib(n-1) + fib(n-2)
 
-print([fib(n) for n in range(10)])
+fib(10)
