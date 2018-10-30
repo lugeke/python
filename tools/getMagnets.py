bebc43a8ce55b078304178d644f0b5ca8501eece
jiaheng <lugeke@gmail.com>
2018-10-30 11:00:58 +0800

    rename genetor to generator

--- tools/getMagnets.py
+++ tools/getMagnets.py
@@ -2,21 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from collections import namedtuple
+import urllib
 # url => html => content
 url = 'https://www.bthuahua.net/index.php?r=files%2Findex&kw=Riley+Reyes+rarbg+1080p&page={}'
 # url = 'https://bt2.bt87.cc/index.php?r=files%2Findex&kw=Nina+Skye+rarbg+1080p&page={}'
 
 MagnetURI = namedtuple('MagnetURI', ['name', 'hashcode', 'size'])
 
-def url_genetor(url, start=1, end=100):
+def url_generator(url, start=1, end=100):
     for i in range(start, end+1):
         yield url.format(i)
 
-def html_genetor(urls):
+def html_generator(urls):
     for url in urls:
         yield requests.get(url).text
 
-def content_genetor(htmls):
+def content_generator(htmls):
     for h in htmls:
         soup = BeautifulSoup(h, 'html.parser')
         lis = soup.find_all('li', class_='col-xs-12 list-group-item')
@@ -28,9 +29,9 @@ def content_genetor(htmls):
             magnet = MagnetURI(name, hashcode, size)
             yield magnet
 
-urls = url_genetor(url)
-htmls = html_genetor(urls)
-contents = content_genetor(htmls)
+urls = url_generator(url)
+htmls = html_generator(urls)
+contents = content_generator(htmls)
 
 
 for m in contents:
